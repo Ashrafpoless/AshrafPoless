1f4cb01cf22df4dfdbcf4e14e88ed80171483cb2
Ashrafpoless <150967649+Ashrafpoless@users.noreply.github.com>
2025-05-17 00:17:28 +0200

    img

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  //output: 'export',
+  //loader: 'custom',
+  //loaderFile: './loader.js',
   images: {
-    loader: 'custom',
-    loaderFile: './loader.js',
     remotePatterns: [
       {
         protocol: "https",
